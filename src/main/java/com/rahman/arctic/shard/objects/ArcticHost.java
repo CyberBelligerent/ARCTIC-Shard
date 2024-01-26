0d54213a6ed888eb4a4467317a5bd20fe121f1e8
CyberBelligerent <157746921+CyberBelligerent@users.noreply.github.com>
2024-01-26 18:36:46 +0100

    Base Object Adding
    Base objects have been added to serve as a wrapper for different provider objects. Some more works needs to be done for this to fully work

---
+++ src/main/java/com/rahman/arctic/shard/objects/ArcticHost.java
@@ -0,0 +1,15 @@
+package com.rahman.arctic.shard.objects;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import lombok.Data;
+
+@Data
+public class ArcticHost {
+	private String ip;
+	private String name;
+	private String flavor;
+	private Set<String> networks = new HashSet<>();
+	private Set<String> volumes = new HashSet<>();
+}
---
+++ src/main/java/com/rahman/arctic/shard/objects/ArcticNetwork.java
@@ -0,0 +1,8 @@
+package com.rahman.arctic.shard.objects;
+
+import lombok.Data;
+
+@Data
+public class ArcticNetwork {
+	private String name;
+}

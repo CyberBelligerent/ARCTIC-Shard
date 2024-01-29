ad7b4e851df38df799996964694265e17ce5f1c6
CyberBelligerent <157746921+CyberBelligerent@users.noreply.github.com>
2024-01-29 11:45:16 +0100

    Generic Waiter
    A generic waiting class has been given to help with ensuring the programmer adds the waiter needed for the client and object with the ArcticTask

---
+++ src/main/java/com/rahman/arctic/shard/shards/Waiter.java
@@ -0,0 +1,26 @@
+package com.rahman.arctic.shard.shards;
+
+import com.rahman.arctic.shard.exceptions.ResourceErrorException;
+import com.rahman.arctic.shard.exceptions.ResourceTimeoutException;
+
+/**
+ * Interface to create the method for waiting on OpenStack resources to be created
+ * @author SGT Rahman
+ * 
+ * @param <T> Type of client
+ * @param <R> Type to be waiting on
+ */
+public interface Waiter<T, R> {
+	/**
+	 * Will stop the current thread until the resource becomes available or errors out
+	 * @param client OpenStack client to obtain information about the resource
+	 * @param re RangeExercise ID for sending information to
+	 * @param resource Resource to be waited on
+	 * @param timeInSeconds TimeOut in seconds
+	 * @param pollingTimeInSeconds Time between checking on status of resource
+	 * @return If the wait was successful or not
+	 * @throws ResourceTimeoutException If the resource was not able to be created in time
+	 * @throws ResourceErrorException If the resource had an error from OpenStack
+	 */
+	boolean waitUntilReady(T client, String re, R resource, int timeInSeconds, int pollingTimeInSeconds) throws ResourceTimeoutException, ResourceErrorException;
+}

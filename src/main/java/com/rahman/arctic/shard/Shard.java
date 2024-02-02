50189b77be4f15581507b482502b661a5e5ecaec
CyberBelligerent <157746921+CyberBelligerent@users.noreply.github.com>
2024-02-02 21:03:48 +0100

    ShardManager object Update
    ShardManager is now an obtainable object that can be passed to Iceberg for use when creating ranges

--- src/main/java/com/rahman/arctic/shard/Shard.java
+++ src/main/java/com/rahman/arctic/shard/Shard.java
@@ -4,13 +4,18 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import lombok.Getter;
+
 @Configuration
 @EnableJpaRepositories(basePackages = {"com.rahman.arctic.shard.repos"})
 @EntityScan("com.rahman.arctic.shard.objects")
 public class Shard {
 
+	@Getter
+	private ShardManager shardManager;
+	
 	public Shard() {
-		new ShardManager("./.providers");
+		shardManager = new ShardManager("./.providers");
 		System.out.println("Enabling Service: Shard");
 	}
 	

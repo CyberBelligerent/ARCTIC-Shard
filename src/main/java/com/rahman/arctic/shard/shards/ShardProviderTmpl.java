b831255a88591de6fd1405ced36dce169bfb725b
CyberBelligerent <157746921+CyberBelligerent@users.noreply.github.com>
2024-02-18 17:51:45 +0100

    Name Change
    Changed all Shard Objects to their proper names

--- src/main/java/com/rahman/arctic/shard/shards/ShardProviderTmpl.java
+++ src/main/java/com/rahman/arctic/shard/shards/ShardProviderTmpl.java
@@ -6,13 +6,13 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.rahman.arctic.shard.ShardManager;
-import com.rahman.arctic.shard.objects.ArcticHost;
-import com.rahman.arctic.shard.objects.ArcticNetwork;
-import com.rahman.arctic.shard.objects.ArcticRouter;
-import com.rahman.arctic.shard.objects.ArcticSecurityGroup;
-import com.rahman.arctic.shard.objects.ArcticSecurityGroupRule;
+import com.rahman.arctic.shard.objects.ArcticHostSO;
+import com.rahman.arctic.shard.objects.ArcticNetworkSO;
+import com.rahman.arctic.shard.objects.ArcticRouterSO;
+import com.rahman.arctic.shard.objects.ArcticSecurityGroupSO;
+import com.rahman.arctic.shard.objects.ArcticSecurityGroupRuleSO;
 import com.rahman.arctic.shard.objects.ArcticTask;
-import com.rahman.arctic.shard.objects.ArcticVolume;
+import com.rahman.arctic.shard.objects.ArcticVolumeSO;
 import com.rahman.arctic.shard.util.ProfileProperties;
 
 import lombok.Getter;
@@ -53,36 +53,36 @@ public abstract class ShardProviderTmpl<T> {
 	
 	public abstract T createClient();
 	
-	public void createHost(ArcticHost ah) {
+	public void createHost(ArcticHostSO ah) {
 		instanceTasks.put(ah.getName(), buildHost(ah));
 	}
 	
-	public void createNetwork(ArcticNetwork an) {
+	public void createNetwork(ArcticNetworkSO an) {
 		networkTasks.put(an.getName(), buildNetwork(an));
 	}
 	
-	public void createSecurityGroup(ArcticSecurityGroup asg) {
+	public void createSecurityGroup(ArcticSecurityGroupSO asg) {
 		securityGroupTasks.put(asg.getName(), buildSecurityGroup(asg));
 	}
 	
-	public void createSecurityGroupRule(ArcticSecurityGroupRule asgr) {
+	public void createSecurityGroupRule(ArcticSecurityGroupRuleSO asgr) {
 		securityGroupRuleTasks.put(asgr.getName(), buildSecurityGroupRule(asgr));
 	}
 	
-	public void createRouter(ArcticRouter ar) {
+	public void createRouter(ArcticRouterSO ar) {
 		routerTasks.put(ar.getName(), buildRouter(ar));
 	}
 	
-	public void createVolume(ArcticVolume av) {
+	public void createVolume(ArcticVolumeSO av) {
 		volumeTasks.put(av.getName(), buildVolume(av));
 	}
 	
-	protected abstract ArcticTask<T,?> buildHost(ArcticHost ah);
-	protected abstract ArcticTask<T,?> buildNetwork(ArcticNetwork an);
-	protected abstract ArcticTask<T,?> buildSecurityGroup(ArcticSecurityGroup asg);
-	protected abstract ArcticTask<T,?> buildSecurityGroupRule(ArcticSecurityGroupRule asgr);
-	protected abstract ArcticTask<T,?> buildRouter(ArcticRouter ar);
-	protected abstract ArcticTask<T,?> buildVolume(ArcticVolume av);
+	protected abstract ArcticTask<T,?> buildHost(ArcticHostSO ah);
+	protected abstract ArcticTask<T,?> buildNetwork(ArcticNetworkSO an);
+	protected abstract ArcticTask<T,?> buildSecurityGroup(ArcticSecurityGroupSO asg);
+	protected abstract ArcticTask<T,?> buildSecurityGroupRule(ArcticSecurityGroupRuleSO asgr);
+	protected abstract ArcticTask<T,?> buildRouter(ArcticRouterSO ar);
+	protected abstract ArcticTask<T,?> buildVolume(ArcticVolumeSO av);
 	
 	private ProfileProperties loadSettings(String domain) {
 		return sManager.getShardProperties().get(domain);

b2a27ee3806a6fbbd5bb90633961c055d2a0c8ca
CyberBelligerent <157746921+CyberBelligerent@users.noreply.github.com>
2024-02-18 17:50:43 +0100

    Repo Deletion
    Shard will no longer store the repositories for Arctic Objects. That will be handled by Iceberg. Iceberg will create and send over the SO objects to Shard when needed.

--- src/main/java/com/rahman/arctic/shard/repos/ArcticHostRepo.java
+++
@@ -1,9 +0,0 @@
-package com.rahman.arctic.shard.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.rahman.arctic.shard.objects.ArcticHost;
-
-@Repository
-public interface ArcticHostRepo extends JpaRepository<ArcticHost, String>{}
--- src/main/java/com/rahman/arctic/shard/repos/ArcticVolumeRepo.java
+++
@@ -1,9 +0,0 @@
-package com.rahman.arctic.shard.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.rahman.arctic.shard.objects.ArcticVolume;
-
-@Repository
-public interface ArcticVolumeRepo extends JpaRepository<ArcticVolume, String>{}

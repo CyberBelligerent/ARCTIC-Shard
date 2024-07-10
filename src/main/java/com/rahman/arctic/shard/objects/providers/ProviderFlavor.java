69147acaeb8bc3ab3bf3227b9dacf28a59fe778c
CyberBelligerent <157746921+CyberBelligerent@users.noreply.github.com>
2024-07-10 14:25:28 +0200

    Provider classes
    Added 2 new classes:
    - ProviderImage
    - ProviderFlavor
    
    These classes will be used with the Provider to understand where to grab the Images and Flavors for VM creation

---
+++ src/main/java/com/rahman/arctic/shard/objects/providers/ProviderFlavor.java
@@ -0,0 +1,13 @@
+package com.rahman.arctic.shard.objects.providers;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ProviderFlavor {
+
+	private String id;
+	private String name;
+	
+}

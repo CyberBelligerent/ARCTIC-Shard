9701b986f1efdd18f9fa23950e241b6cb465c6f1
CyberBelligerent <157746921+CyberBelligerent@users.noreply.github.com>
2024-07-10 14:27:12 +0200

    BEANS
    Added ShardManager as a publicly available Bean that can be accessed by other modules

--- src/main/java/com/rahman/arctic/shard/Shard.java
+++ src/main/java/com/rahman/arctic/shard/Shard.java
@@ -1,6 +1,7 @@
 package com.rahman.arctic.shard;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -18,5 +19,10 @@ public class Shard {
 		shardManager = new ShardManager("./.providers");
 		System.out.println("Enabling Service: Shard");
 	}
+	
+	@Bean
+    public ShardManager shardManager() {
+        return this.shardManager;
+    }
 	
 }

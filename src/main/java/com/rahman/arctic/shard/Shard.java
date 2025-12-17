015b21fbb267f7e5bd80c11663d20b38c616f60e
Sean Rahman <157746921+CyberBelligerent@users.noreply.github.com>
2025-12-17 22:23:50 +0100

    Annotation and Spring Boot Fix
    Fixed Shard during entity scanning and jpa repository searching. This is controlled by Flock now.
    
    Also, ensured that ShardManager is not created by Shard. ShardManager is officially controlled by the Spring Boot life cycle.

--- src/main/java/com/rahman/arctic/shard/Shard.java
+++ src/main/java/com/rahman/arctic/shard/Shard.java
@@ -1,28 +1,12 @@
 package com.rahman.arctic.shard;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Service;
 
-import lombok.Getter;
-
-@Configuration
-@EnableJpaRepositories(basePackages = {"com.rahman.arctic.shard.repos"})
-@EntityScan("com.rahman.arctic.shard.objects")
+@Service
 public class Shard {
 
-	@Getter
-	private ShardManager shardManager;
-	
 	public Shard() {
-		shardManager = new ShardManager("./.providers");
 		System.out.println("Enabling Service: Shard");
 	}
-	
-	@Bean
-    public ShardManager shardManager() {
-        return this.shardManager;
-    }
-	
+
 }

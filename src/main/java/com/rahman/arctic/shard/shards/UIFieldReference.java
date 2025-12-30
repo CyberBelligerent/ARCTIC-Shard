23d93564c96ed2042404ac8db3d952de37fea6f9
Sean Rahman <157746921+CyberBelligerent@users.noreply.github.com>
2025-12-30 13:07:47 +0100

    UIFieldReference
    Added a nice POJO reference for UIFields so that a static item can be returned.

---
+++ src/main/java/com/rahman/arctic/shard/shards/UIFieldReference.java
@@ -0,0 +1,10 @@
+package com.rahman.arctic.shard.shards;
+
+import lombok.Data;
+
+@Data
+public class UIFieldReference {
+	private String key;
+	private String label;
+	private Object returnValue;
+}

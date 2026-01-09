3fd51f460432365ac477bdc0249455bdcd54fffe
Sean Rahman <157746921+CyberBelligerent@users.noreply.github.com>
2026-01-09 10:29:06 +0100

    Removed OpenStack Specific Variables
    Removed instances of hardcoded OpenStack specific variables. Those will be moved into ExtraVariables instead.

--- src/main/java/com/rahman/arctic/shard/objects/abstraction/ArcticHostSO.java
+++ src/main/java/com/rahman/arctic/shard/objects/abstraction/ArcticHostSO.java
@@ -1,6 +1,8 @@
 package com.rahman.arctic.shard.objects.abstraction;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import lombok.Data;
@@ -10,13 +12,15 @@ public class ArcticHostSO {
 	private String ip;
 	private String providerId;
 	private String name;
-	private String defaultUser;
-	private String defaultPassword;
-	private String flavor;
+//	private String defaultUser;
+//	private String defaultPassword;
+//	private String flavor;
 	private String rangeId;
-	private String imageId;
+//	private String imageId;
 	private String osType;
-	private Set<String> wantedIPs = new HashSet<>();
+//	private Set<String> wantedIPs = new HashSet<>();
 	private Set<String> networks = new HashSet<>();
 	private Set<String> volumes = new HashSet<>();
+	
+	private Map<String, String> extraVariables = new HashMap<>();
 }
